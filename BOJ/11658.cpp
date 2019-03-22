ae23b6646c27cd6ec92c4acfeba5e8b269d90276
Ryuis <ryuis1124@gmail.com>
2019-03-22 15:27:51 +0900

    segment tree and fenwick tree

--- BOJ/11658.cpp
+++ BOJ/11658.cpp
@@ -7,7 +7,7 @@
 #define MAX_SIZE 1024
 using namespace std;
 
-int arr[MAX_SIZE][MAX_SIZE];
+/* sum[i][j] denotes the sum of sum[i][0] ~ sum[i][j] */
 int sum[MAX_SIZE][MAX_SIZE];
 
 int n, m;
@@ -18,32 +18,37 @@ int main()
 	cin.tie(NULL);
 
 	cin >> n >> m;
-
-	for(int i = 0; i < n; i++)
-		for (int j = 0; j < n; j++)
-			cin >> arr[i][j];
-	
+	int temp;
 	for (int i = 0; i < n; i++)
-	{	
-		sum[i][0] = arr[i][0];
+	{
+		cin >> sum[i][0];
 		for (int j = 1; j < n; j++)
-			sum[i][j] = arr[i][j] + sum[i][j - 1];
+		{
+			cin >> temp;
+			sum[i][j] = temp + sum[i][j - 1];
+		}
 	}
+	
 	int a, b, c, d;
 
 	for (int i = 0; i < m; i++)
 	{
 		cin >> a >> b >> c >> d;
 		b--; c--;
+		/*update*/
 		if (a == 0)
-		{
-			int diff = d - arr[b][c];
-			arr[b][c] = d;
+		{	
+			int diff;
+			if (c == 0)
+				diff = d - sum[b][c];
+			else
+				diff = d - (sum[b][c] - sum[b][c - 1]);
 
 			for (int j = c; j < n; j++)
 				sum[b][j] += diff;
 				
 		}
+		/*sum*/
 		else
 		{
 			int e;
@@ -69,4 +74,80 @@ int main()
 	}
 
 	return 0;
+}
+/*
+using fenwick tree 
+https://www.acmicpc.net/blog/view/21
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int tree[1025][1025];
+int n, m;
+
+int sum(int i, int j);
+void update(int i, int j, int val);
+
+int main() 
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> n >> m;
+
+	int val;
+	for(int i = 1; i <= n; i++)
+		for (int j = 1; j <= n; j++)
+		{
+			cin >> val;
+			update(i, j, val);
+		}
+
+	int a;
+	for (int i = 0; i < m; i++)
+	{
+		cin >> a;
+
+		if (a == 0)
+		{
+			int r, c;
+			cin >> r >> c >> val;
+
+			int original = sum(r, c) - sum(r - 1, c) - sum(r, c - 1) + sum(r - 1, c - 1);
+
+			update(r, c, val - original);
+
+		}
+		else 
+		{
+			int r1, c1, r2, c2;
+			cin >> r1 >> c1 >> r2 >> c2;
+
+			cout << sum(r2, c2) - sum(r1 - 1, c2) - sum(r2, c1 - 1) + sum(r1 - 1, c1 - 1) << "\n";
+		}
+
+	}
+
+	return 0;
+}
+
+
+void update(int r, int c, int val)
+{
+	for (int i = r; i <= n; i += (i & -i))
+		for (int j = c; j <= n; j += (j & -j))
+			tree[i][j] += val;
+}
+
+int sum(int r, int c)
+{
+	int ans = 0;
+
+	for (int i = r; i > 0; i -= (i & -i))
+		for (int j = c; j > 0; j -= (j & -j))
+			ans += tree[i][j];
+
+	return ans;
 }
